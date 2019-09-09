92c7e60243d635f881954d81964d6de85eeee90f
Joerg-Christian Boehme <joerg@chaosdorf.de>
2019-09-09 05:09:24 +0200

    Improve sdk startup

---
+++ sdk/env.sh
@@ -0,0 +1,4 @@
+groupmod -g ${GROUP_ID} build
+usermod -u ${USER_ID} -g ${GROUP_ID} build
+export DISPLAY=$DISPLAY
+su -w DISPLAY -l build /home/build/sdk/startup.sh
--- set-environment.sh
+++
@@ -1,4 +0,0 @@
-groupmod -g ${GROUP_ID} build
-usermod -u ${USER_ID} -g ${GROUP_ID} build
-export DISPLAY=$DISPLAY
-su -w DISPLAY -l build
