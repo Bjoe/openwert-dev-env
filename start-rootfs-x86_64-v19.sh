83b319e3a53dede30935ef39e747c88b1283556c
Joerg-Christian Boehme <joerg@chaosdorf.de>
2020-04-14 17:41:29 +0200

    Fix rootfs start scripts

--- start-rootfs-x86_64-v19.sh
+++ start-rootfs-x86_64-v19.sh
@@ -5,6 +5,6 @@ echo "Install additional pkg after running the rootfs"
 echo "opkg update && opkg install libstdcpp6 gdb gdbserver openssh-sftp-server"
 echo "==============================================================================="
 export DOCKER_SERVICE=openwrt-rootfs
-export DOCKER_CONTAINER=openwrt-rootfs
+export DOCKER_CONTAINER=openwrt-rootfs-x86_64-v19
 export DOCKER_COMPOSE_FILE=docker-compose.x86_64-v19.yml
 ./scripts/run.sh $@
